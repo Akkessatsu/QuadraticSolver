068cc35a007da1a0b6633c32cd1996b8f74b49ae
Akkessatsu <99400178+Akkessatsu@users.noreply.github.com>
2023-06-28 23:05:43 -0300

    Update QuadraticSolver.py

--- QuadraticSolver.py
+++ QuadraticSolver.py
@@ -1,160 +1,153 @@
-import cmath
 import math
+import cmath
+from abc import ABC, abstractmethod
 
-class QuadraticEquation():
-    def __init__(self, a: int, b: int, c: int):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.delta: float = None
-        self.typeOfRoots: str = None
-        self.roots = []
+class Quadratic_Equation_Elements():
+    def __init__(self):
+        self.a = int(input("Insira o valor do coeficiente a: "))
+        self.b = int(input("Insira o valor do coeficiente b: "))
+        self.c = int(input("Insira o valor do coeficiente c: "))
+        self.delta: float = 0.0
+        self.solution_type: int = 0
+        self.roots: list = []
+        self.vertice: tuple = ()
+
+class Delta(ABC):
+    @abstractmethod
+    def delta (self, equation: type[Quadratic_Equation_Elements]) -> float:
+        pass
+    
+class Roots(ABC):
+    @abstractmethod
+    def roots(self, equation: type[Quadratic_Equation_Elements]) -> float:
+        pass
+class Check_Roots_Type(ABC):
+    @abstractmethod
+    def check_roots_type (self, equation: type[Quadratic_Equation_Elements]) -> int:
+        pass
+class Vertice_Coordinates(ABC):
+    @abstractmethod
+    def x_vertice_coordinate (self, equation: type[Quadratic_Equation_Elements]) -> float:
+        pass
+    
+    @abstractmethod
+    def y_vertice_coordinante (self, equation: type[Quadratic_Equation_Elements]) -> float:
+        pass
+    
+class Quadratic_Equation(Quadratic_Equation_Elements):
+    def __init__(self):
+        super().__init__()
         
-    def show_equation(self) -> str:
-        return f"{self.getA()}x^2 + {self.getB()}x + {self.getC()} = 0"
-        
-    def setDelta(self, delta: float) -> None:
-        self.delta = delta
-        
-    def setRoot(self, root: float) -> None:
-        self.roots.append(root)
-        
-    def setTypeOfRoots(self, delta: float) -> None:
-        if delta < 0:
-            self.typeOfRoots = "COMPLEX"
-        elif delta > 0:
-            self.typeOfRoots = "TWO_ROOTS"
-        else:
-            self.typeOfRoots = "DOUBLE_ROOTS"
-        
-    def getA(self) -> float:
+    def get_A(self):
         return self.a
     
-    def getB(self) -> float:
+    def get_B(self):
         return self.b
     
-    def getC(self) -> float:
+    def get_C(self):
         return self.c
     
-    def getDelta(self) -> float:
+    def get_delta(self):
         return self.delta
     
-    def getRoots(self) -> list:
+    def get_roots(self):
         return self.roots
     
-    def getTypeOfRoots(self) -> str:
-        return self.typeOfRoots
+    def get_vertice(self):
+        return self.vertice
     
+    def get_solution_type(self):
+        return self.solution_type
+    
+    def set_delta(self, delta) -> None:
+        self.delta = delta
+    
+    def set_solution_type(self, solution_type: int) -> None:
+        self.solution_type = solution_type
+    
+    def set_roots(self, roots: list) -> None:
+        self.roots = roots
+    
+    def set_vertice(self, vertice: tuple) -> None:
+        self.vertice = vertice
 
-class QuadraticSolver():
-    def __init__(self):
-        print("Calculadora de equacoes quadraticas!")
-        self.equation: type[QuadraticEquation] = None
+class Vertice(Vertice_Coordinates):
+    def x_vertice_coordinate(self, equation: type[Quadratic_Equation]) -> float:
+        a = equation.get_A()
+        b = equation.get_B()
         
-    def __setEquation(self, a, b, c) -> None:
-        self.equation = QuadraticEquation(a, b, c)
-        return
-        
-    def __getEquation(self) -> type[QuadraticEquation]:
-        return self.equation
+        return -b/(2 * a)
     
-    def inputEquation(self) -> type[QuadraticEquation]:
-        print("Insira o valor dos coeficientes da equacao quadratica\n")
-        a = 0
-        while a == 0:
-            a = int(input("Insira o valor de a: "))
-            b = int(input("Insira o valor de b: "))
-            c = int(input("Insira o valor de c: "))
-            
-            self.__checkInput(a, b, c)
-            
-    def __checkInput(self,a: int, b: int, c: int) -> None:
-        if a != 0:
-            self.__setEquation(a, b, c)
-        else:
-            Exception("Insira um valor de a válido!")
+    def y_vertice_coordinante(self, equation: type[Quadratic_Equation]) -> float:
+        delta = equation.get_delta()
+        a = equation.get_A()
+        
+        return -delta/(4 * a)
     
-    def __complexRootsSolution(self) -> list:
-        equationObj = solver.__getEquation()
-        
-        a = equationObj.getA()
-        b = equationObj.getB()
-        delta = equationObj.getDelta()
-        
-        root1 = (-b + cmath.sqrt(delta))/ (2 * a)
-        root2 = (-b - cmath.sqrt(delta))/ (2 * a)
-        
-        equationObj.setRoot(root1)
-        equationObj.setRoot(root2)
-        
-        return equationObj.getRoots()
-    
-    def __twoRootsSolution(self) -> list:
-        equationObj = solver.__getEquation()
-        
-        a = equationObj.getA()
-        b = equationObj.getB()
-        delta = equationObj.getDelta()
-    
-        root1 = (-b + math.sqrt(delta))/ (2 * a)
-        root2 = (-b - math.sqrt(delta))/ (2 * a)
-        
-        equationObj.setRoot(root1)
-        equationObj.setRoot(root2)
-        
-        return equationObj.getRoots()
-    
-    def __doubleRootSolution(self) -> list:
-        equationObj = solver.__getEquation()
-        
-        a = equationObj.getA()
-        b = equationObj.getB()
-        
-        root = -b / (2 * a)
-        
-        equationObj.setRoot(root)
-        
-        return equationObj.getRoots()
-    
-    def delta(self) -> float:
-        equationObj = solver.__getEquation()
-        
-        a = equationObj.getA()
-        b = equationObj.getB()
-        c = equationObj.getC()
-        
-        delta = (b ** 2) -4 * (a) * (c)
-        equationObj.setDelta(delta)
-        equationObj.setTypeOfRoots(delta)
-        
-        return self.__getEquation().getDelta()
-    
-    def roots(self) -> list:
-        equationObj = solver.__getEquation()
-        
-        typeOfRoot = equationObj.getTypeOfRoots()
-    
-        if typeOfRoot == "COMPLEX":
-            self.__complexRootsSolution()
-        elif typeOfRoot == "TWO_ROOTS":
-            self.__twoRootsSolution()
-        else:
-            self.__doubleRootSolution()
-            
-    def show_roots(self) -> None:
-        equationObj = solver.__getEquation()
-        
-        roots = equationObj.getRoots()
-        equation = equationObj.show_equation()
-        
-        print(f"\nA equacao {equation} tem como raizes:")
-        for index, root in enumerate(roots):
-            print(f"({index + 1}º) {root}")
-            
-solver = QuadraticSolver()
-solver.inputEquation()
-solver.delta()
-solver.roots()
-solver.show_roots()
+    def vertice_coordinate(self, equation: type[Quadratic_Equation]) -> tuple:
+        return(self.x_vertice_coordinate(equation), self.y_vertice_coordinante(equation))
 
+class Check_Roots(Check_Roots_Type):
+    def check_roots_type(self, equation: type[Quadratic_Equation]) -> int:
+        delta = equation.get_delta()
+        
+        if delta > 0:
+            return 2
+        elif delta < 0:
+            return 1
+        else:
+            return 0
             
+class Eval_Roots(Roots):
+    def roots(self, equation: type[Quadratic_Equation]) -> tuple:
+        a = equation.get_A()
+        b = equation.get_B()
+        delta = equation.get_delta()
+        solution_type = equation.get_solution_type()
+        
+        match(solution_type):
+            case 2:
+                return (
+                    (-b + math.sqrt(delta))/(2 * a),
+                    (-b - math.sqrt(delta))/(2 * a)
+                        )
+            case 1:
+                return (
+                    (-b + cmath.sqrt(delta)/(2 * a)),
+                    (-b + cmath.sqrt(delta)/(2 * a))  
+                )
+            case 0:
+                return (
+                    -b / (2 * a)
+                )
+                
+class Eval_Delta(Delta):
+    def delta(self, equation: type[Quadratic_Equation]) -> float:
+        a = equation.get_A()
+        b = equation.get_B()
+        c = equation.get_C()
+        
+        return (b ** 2) + (-4 * a * c)
+
+class Display():
+    def __init__(self, equation: type[Quadratic_Equation]):
+        self.equation = equation
+    
+    def print_results(self):
+        print(f"\nAs raízes são: {self.equation.get_roots()}")
+        print(f"O valor do discriminante é: {self.equation.get_delta()}")
+        print(f"O vértice da parábola é: {self.equation.get_vertice()}")
+
+vertice_solver = Vertice()
+delta_solver = Eval_Delta()
+solution_checker = Check_Roots()
+root_solver = Eval_Roots()
+quadratic_equation = Quadratic_Equation()
+
+quadratic_equation.set_delta(delta_solver.delta(quadratic_equation))
+quadratic_equation.set_solution_type(solution_checker.check_roots_type(quadratic_equation))
+quadratic_equation.set_roots(root_solver.roots(quadratic_equation))
+quadratic_equation.set_vertice(vertice_solver.vertice_coordinate(quadratic_equation))
+
+display = Display(quadratic_equation)
+display.print_results()
